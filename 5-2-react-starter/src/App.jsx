e33146995476e53eb920d0bb8eb5280cbf6aef5b
Almualim <s202245200@ccm.kfupm.edu.sa>
2025-10-01 09:28:24 +0300

    Finished Task

--- 5-2-react-starter/src/App.jsx
+++ 5-2-react-starter/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-
+import StudentCard from "C:\\Users\\s202245200\\Downloads\\react-starter\\src\\components/StudentCard.jsx"
 function App() {
   return (
     <div className="app">
@@ -10,7 +10,9 @@ function App() {
 
       <main className="dashboard-main">
         <div className="cards-container">
-          {/* TODO: Import and render StudentCard components here */}
+          {
+            <StudentCard name="Hasan Almualim" sid="202245200" dep="ICS"></StudentCard>
+          /* TODO: Import and render StudentCard components here */}
         </div>
       </main>
     </div>
